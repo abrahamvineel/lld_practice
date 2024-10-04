ee43b061a16e1f0fc3bcd6f82683fc974d51a938
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-10-04 18:03:47 -0400

    idle state init

---
+++ vending_machine/vending_machine/src/main/java/org/example/vendingmachinestate/IdleState.java
@@ -0,0 +1,18 @@
+package org.example.vendingmachinestate;
+
+public class IdleState implements VendingMachineState{
+    @Override
+    public void insertMoney(VendingMachineContext context, double amount) {
+
+    }
+
+    @Override
+    public void selectProduct(VendingMachineContext context, String product) {
+
+    }
+
+    @Override
+    public void dispenseProduct(VendingMachineContext context) {
+
+    }
+}
--- vending_machine/vending_machine/src/main/java/org/example/vendingmachinestate/VendingMachineContext.java
+++ vending_machine/vending_machine/src/main/java/org/example/vendingmachinestate/VendingMachineContext.java
@@ -6,5 +6,27 @@ public class VendingMachineContext {
     private double balance;
     private String selectedProduct;
 
+    public VendingMachineState getCurrentState() {
+        return currentState;
+    }
 
+    public void setCurrentState(VendingMachineState currentState) {
+        this.currentState = currentState;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getSelectedProduct() {
+        return selectedProduct;
+    }
+
+    public void setSelectedProduct(String selectedProduct) {
+        this.selectedProduct = selectedProduct;
+    }
 }
