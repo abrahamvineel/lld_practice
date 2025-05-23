686a8e61e3f6b47dc63e0ba79bfdd3a60288b343
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2025-05-22 20:07:28 -0400

    Update Product.java

--- vending_machine/vending_machine/src/main/java/org/example/Product.java
+++ vending_machine/vending_machine/src/main/java/org/example/Product.java
@@ -7,7 +7,6 @@ public class Product {
     private ProductType type;
     private int quantity;
 
-
     public Product(String name, double price, ProductType type, int quantity) {
         this.name = name;
         this.price = price;
