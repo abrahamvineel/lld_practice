b704a2a295b8f214f6ceb324e107b4b25cfd6023
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2025-07-02 19:47:03 -0400

    Update Chocolate.java

--- vending_machine/vending_machine/src/main/java/org/example/Chocolate.java
+++ vending_machine/vending_machine/src/main/java/org/example/Chocolate.java
@@ -1,5 +1,6 @@
 package org.example;
 
+
 public class Chocolate extends Product{
     public Chocolate(String name, double price, int quantity) {
         super(name, price, ProductType.CHOCOLATE, quantity);
