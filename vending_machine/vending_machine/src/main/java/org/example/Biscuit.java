5665c0cc34416f9ce2378760f51e3f60743d9c22
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2025-07-29 20:15:10 -0400

    Update Biscuit.java

--- vending_machine/vending_machine/src/main/java/org/example/Biscuit.java
+++ vending_machine/vending_machine/src/main/java/org/example/Biscuit.java
@@ -2,7 +2,6 @@ package org.example;
 
 public class Biscuit extends Product{
     public Biscuit(String name, double price, int quantity) {
-        
         super(name, price, ProductType.BISCUIT, quantity);
     }
 }
