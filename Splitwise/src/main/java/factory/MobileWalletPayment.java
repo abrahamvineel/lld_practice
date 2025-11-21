0e86cbaf7105be8e6903eef86ab2c48a0e4b33a9
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2025-11-21 12:37:03 -0500

    Update MobileWalletPayment.java

--- Splitwise/src/main/java/factory/MobileWalletPayment.java
+++ Splitwise/src/main/java/factory/MobileWalletPayment.java
@@ -2,7 +2,6 @@ package factory;
 
 public class MobileWalletPayment implements Payment {
 
-
     @Override
     public void processPayment(double amount) {
         System.out.println(amount + " paid using mobile wallet");
