a1ab6ecf1974a011c971827384201398f8b9a6d1
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2026-01-31 21:19:40 -0500

    Fix output message format in processPayment method

--- Splitwise/src/main/java/factory/MobileWalletPayment.java
+++ Splitwise/src/main/java/factory/MobileWalletPayment.java
@@ -4,6 +4,6 @@ public class MobileWalletPayment implements Payment {
 
     @Override
     public void processPayment(double amount) {
-        System.out.println(amount + " paid using mobile wallet");
+        System.out.println(amount + " paid using mobile wallet.");
     }
 }
